e19156d77f87037cdb096a4f6bf86378d5dd3ef9
archdev <dev@archlinux.com>
2021-05-03 14:57:12 +0800

    change route to object

--- http/router.js
+++ http/router.js
@@ -55,10 +55,8 @@ const init_router = function (meta) {
         init_delete_router(router, meta_entity);
     }
 
-    if (meta.routes) {
-        meta.routes.forEach(route => {
-            route(router, meta_entity);
-        });
+    if (meta.route) {
+        meta.route(router, meta_entity);
     }
 
     return router;
