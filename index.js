c21bd13cf428e7099a4c04c8abd16d154dd1bc88
Heyuan Liu <heyuan.liu@intel.com>
2023-06-07 18:21:29 +0800

    export is_root_role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { get_db } = require('./db/db');
 const { Entity } = require('./db/entity');
-const { init_settings } = require('./setting');
+const { init_settings, is_root_role } = require('./setting');
 const { init_router } = require('./http/router');
 const { init_express_server } = require('./http/express');
 const { register_type, get_type } = require('./core/type');
@@ -30,7 +30,7 @@ const { gen_i18n } = require('./tool/gen_i18n');
 const { log_debug, log_info, log_warn, log_error, is_log_debug, is_log_info, is_log_warn, is_log_error, get_session_userid, oid_queries, oid_query } = require('./db/db');
 
 module.exports = {
-    init_settings, init_express_server, init_router, register_type, get_type, get_db,
+    init_settings, is_root_role, init_express_server, init_router, register_type, get_type, get_db,
     Entity, EntityMeta, get_entity_meta, array, bash, chart, cron, date, file, lhs, msg, number, obj, random, thread, validate, code, err, params, context, gridfs, gen_i18n,
     log_debug, log_info, log_warn, log_error, is_log_debug, is_log_info, is_log_warn, is_log_error, get_session_userid, oid_queries, oid_query
 };
