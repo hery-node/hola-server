7f16b883144f875373e830dc90766770b5bf194b
hery1979 <hery1979@qq.com>
2023-06-11 14:29:21 +0800

    fix axios 404 throw exception error

--- core/url.js
+++ core/url.js
@@ -5,15 +5,11 @@ const url = function (url, method) {
     const settings = get_settings();
     const axios_config = settings.axios;
 
-    if (axios_config) {
-        axios.defaults.retry = axios_config.retry;
-        axios.defaults.retryDelay = axios_config.retry_delay;
-    }
-
     return function (config) {
         let params = {
             url: url,
-            method: method
+            method: method,
+            validateStatus: false
         };
         if (axios_config.proxy) {
             params.proxy = axios_config.proxy;
--- http/error.js
+++ http/error.js
@@ -6,7 +6,10 @@ const wrap_http = fn => (...args) => fn(...args).catch(args[2]);
 const handle_exception = app => {
     app.use(function (error, req, res, next) {
         if (is_log_error()) {
-            log_error(LOG_SYSTEM, error.stack);
+            if (error) {
+                const error_msg = error.stack ? error.stack : JSON.stringify(error);
+                log_error(LOG_SYSTEM, error_msg);
+            }
         } else {
             throw new Error(error);
         }
